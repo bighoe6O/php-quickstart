f1826fa8968f9ba897ad89232757e7cfda329a3a
Stéphane Peyrucq <s.peyrucq@gmail.com>
2023-12-12 14:42:45 +0100

    insert

--- schema.sql
+++ schema.sql
@@ -7,3 +7,8 @@ CREATE TABLE `rolodex`.`contacts` (
   `email` VARCHAR(100) NOT NULL,
   PRIMARY KEY  (`id`)
 );
+
+USE rolodex;
+
+INSERT INTO `contacts` (`id`, `name`, `age`, `email`) VALUES (NULL, 'Roger Rabbit', '4', 'roger.rabbit@toon.com');
+INSERT INTO `contacts` (`id`, `name`, `age`, `email`) VALUES (NULL, 'Beep Beep', '2', 'beep.beep@toon.com');
